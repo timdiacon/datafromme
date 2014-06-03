3c4b6be80a6ae122a887bdba06e7c54368595982
Tim Diacon <timdiacon@gmail.com>
2014-06-03 17:24:11 +0100

    uncomment Biometric

--- app/controllers/biometricController.js
+++ app/controllers/biometricController.js
@@ -1,23 +1,23 @@
-// var Biometric = require('../models/Biometric');
+var Biometric = require('../models/Biometric');
 
-// module.exports = {
-//     index: function(req, res) {
+module.exports = {
+    index: function(req, res) {
         	
-//         Biometric.find({}, function(err, data) {
-//             res.json(data);
-//         });
+        Biometric.find({}, function(err, data) {
+            res.json(data);
+        });
 
-//     },
+    },
 
-//     add: function(req, res) {
-//         var newBiometric = new Biometric(req.body);
-//         newBiometric.user = req.user._id;
-//         newBiometric.date = new Date();
+    add: function(req, res) {
+        var newBiometric = new Biometric(req.body);
+        newBiometric.user = req.user._id;
+        newBiometric.date = new Date();
 
-//         newBiometric.save(function(err, doc) {
-//             if (err)
-//                 res.json({});
-//             res.json(doc);
-//         });
-//     },
-// };
+        newBiometric.save(function(err, doc) {
+            if (err)
+                res.json({});
+            res.json(doc);
+        });
+    },
+};
--- app/models/biometric.js
+++ app/models/biometric.js
@@ -1,16 +1,16 @@
-// // app/models/Biometric.js
+// app/models/Biometric.js
 
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-// var biometricSchema = mongoose.Schema({
+var biometricSchema = mongoose.Schema({
 
-// 	user: String, // storing ref to user here as other way around could inflate user document to 16mb with huge volumes of data
-// 	date: Date,
+	user: String, // storing ref to user here as other way around could inflate user document to 16mb with huge volumes of data
+	date: Date,
 
-// 	// optional
-// 	weight: Number
+	// optional
+	weight: Number
 
-// });
+});
 
-// // create the model for users and expose it to our app
-// module.exports = mongoose.model('Biometric', biometricSchema);
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Biometric', biometricSchema);
